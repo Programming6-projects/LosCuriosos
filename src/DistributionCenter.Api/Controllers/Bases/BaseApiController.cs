e795be36a15cf9cad2ab93e344498120f3f6d6de
MorveN11 <morales.patty.jose@gmail.com>
2024-09-09 22:39:09 -0400

    chore(*): remove all initial examples

--- src/DistributionCenter.Api/Controllers/Bases/BaseApiController.cs
+++
@@ -1,6 +0,0 @@
-namespace DistributionCenter.Api.Controllers.Bases;
-
-using Microsoft.AspNetCore.Mvc;
-
-[ApiController]
-public class BaseApiController : ControllerBase { }
--- test/DistributionCenter.Api.Tests/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using System.Net;
-global using Microsoft.AspNetCore.Mvc.Testing;
-global using Moq;
-global using Xunit;
