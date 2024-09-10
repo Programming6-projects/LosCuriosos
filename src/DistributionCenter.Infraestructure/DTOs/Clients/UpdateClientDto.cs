b773c8835d22e9238830b834e79877115d7980c5
MorveN11 <morales.patty.jose@gmail.com>
2024-09-09 21:38:36 -0400

    chore: apply rebase for removing initial steps

--- src/DistributionCenter.Infraestructure/DTOs/Clients/UpdateClientDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DistributionCenter.Infraestructure.DTOs.Clients;
-
-public class UpdateClientDto
-{
-    public string? Name { get; init; }
-    public string? LastName { get; init; }
-    public string? Email { get; init; }
-}
