93a26bd323733467122d3c185ba795bd27a5bc5f
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-09-18 16:28:19 -0400

    refactor(*): update test with new implementation

--- test/DistributionCenter.Domain.Tests/Entities/Concretes/TransportTests.cs
+++ test/DistributionCenter.Domain.Tests/Entities/Concretes/TransportTests.cs
@@ -10,25 +10,35 @@ public class TransportTests
     {
         // Define input and expected output
         string expectedName = "Truck A";
-        int expectedCapacity = 100;
-        int expectedAvailableUnits = 50;
+        string expectedPlate = "1852SBJ";
+        int expectedCapacity = 9;
+        int expectedCurrentCapacity = 9;
+        bool expectedIsAvailable = true;
 
         // Create a new instance of Transport
+
+
         Transport entity = new()
         {
-            Name = expectedName,
-            Capacity = expectedCapacity,
-            AvailableUnits = expectedAvailableUnits
+            Name = "Truck A",
+            Plate = "1852SBJ",
+            Capacity = 9,
+            CurrentCapacity = 9,
+            IsAvailable = true
         };
 
         // Execute actual operation
         string name = entity.Name;
+        string plate = entity.Plate;
         int capacity = entity.Capacity;
-        int availableUnits = entity.AvailableUnits;
+        int currentCapacity = entity.CurrentCapacity;
+        bool isAvailable = entity.IsAvailable;
 
         // Verify actual result
         Assert.Equal(expectedName, name);
+        Assert.Equal(expectedPlate, plate);
         Assert.Equal(expectedCapacity, capacity);
-        Assert.Equal(expectedAvailableUnits, availableUnits);
+        Assert.Equal(expectedCurrentCapacity, currentCapacity);
+        Assert.Equal(expectedIsAvailable, isAvailable);
     }
 }
