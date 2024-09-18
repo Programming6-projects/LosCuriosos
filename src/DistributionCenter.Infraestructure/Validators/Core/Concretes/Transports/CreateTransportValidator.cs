57c4c85057e58db6d3195e8b44db581700b3e21c
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-09-18 16:01:18 -0400

    refactor(*): update fluent validation for transports

--- src/DistributionCenter.Infraestructure/Validators/Core/Concretes/Transports/CreateTransportValidator.cs
+++ src/DistributionCenter.Infraestructure/Validators/Core/Concretes/Transports/CreateTransportValidator.cs
@@ -10,17 +10,24 @@ public class CreateTransportValidator : BaseFluentValidator<CreateTransportDto>
     {
         _ = RuleFor(static transport => transport.Name)!
             .WhenNotNull()
-            .SizeRange(1, 64, "Name must be between 3 and 64 characters")
+            .SizeRange(1, 20, "Name must be between 1 and 20 characters")
             .RegexValidator(
                 @"^[a-zA-Z0-9\s]+$",
                 "Name must contain letters, numbers and spaces");
 
+        _ = RuleFor(static transport => transport.Plate)!
+            .WhenNotNull()
+            .SizeRange(4, 7, "Plate must be between 4 and 7 characters")
+            .RegexValidator(
+                @"^[0-9]{1,4}[A-Z]{3}$",
+                    "Plate must contain 1 to 4 numbers and 3 uppercase letters");
+
         _ = RuleFor(static transport => transport.Capacity)
             .NonNegatives("The capacity cannot be a negative number")
-            .NumberRange(1, 2000000, "Capacity must be between 1 and 2,000,000 grams");
+            .NumberRange(500000, 450000000, "Capacity must be between 500.000 and 450.000.000 grams");
 
-        _ = RuleFor(static transport => transport.AvailableUnits)
+        _ = RuleFor(static transport => transport.CurrentCapacity)
             .NonNegatives("Available units cannot be a negative number")
-            .NumberRange(1, 1000, "Capacity must be between 1 and 1000 units");
+            .NumberRange(500000, 450000000, "Current Capacity must be between 500.000 and 450.000.000 grams");
     }
 }
--- src/DistributionCenter.Infraestructure/Validators/Core/Concretes/Transports/UpdateTransportValidator.cs
+++ src/DistributionCenter.Infraestructure/Validators/Core/Concretes/Transports/UpdateTransportValidator.cs
@@ -10,19 +10,29 @@ public class UpdateTransportValidator : BaseFluentValidator<UpdateTransportDto>
     {
         _ = RuleFor(static transport => transport.Name)!
             .WhenNotNull()
-            .SizeRange(1, 64, "Name must be between 3 and 64 characters")
+            .SizeRange(1, 20, "Name must be between 1 and 20 characters")
             .RegexValidator(
-                @"^[a-zA-Z0-9\s\.]+$",
-                "Name must contain letters, decimal, and the following characters: .,-_");
+                @"^[a-zA-Z0-9\s]+$",
+                "Name must contain letters, numbers and spaces");
+
+        _ = RuleFor(static transport => transport.Plate)!
+            .WhenNotNull()
+            .SizeRange(4, 7, "Plate must be between 4 and 7 characters")
+            .RegexValidator(
+                @"^[0-9]{1,4}[A-Z]{3}$",
+                "Plate must contain 1 to 4 numbers and 3 uppercase letters");
 
         _ = RuleFor(static transport => transport.Capacity)
+            .WhenNotNull()
             .NonNegatives("The capacity cannot be a negative number")
-            .NumberRange(1, 2000000, "Capacity must be between 1 and 2,000,000 grams");
+            .NumberRange(500000, 450000000, "Capacity must be between 500.000 and 450.000.000 grams");
 
-        _ = RuleFor(static transport => transport.AvailableUnits)!
+        _ = RuleFor(static transport => transport.CurrentCapacity)
             .WhenNotNull()
             .NonNegatives("Available units cannot be a negative number")
-            .NumberRange(1, 100, "Capacity must be between 1 and 100 units");
+            .NumberRange(500000, 450000000, "Current Capacity must be between 500.000 and 450.000.000 grams");
 
+        _ = RuleFor(static transport => transport.IsAvailable)
+            .WhenNotNull();
     }
 }
