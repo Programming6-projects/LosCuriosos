1520ea3430a0a2df43af25ac87bcae8ecee7c80c
aguirrekarina <karina.aguirre.9491656@usalesiana.edu.bo>
2024-09-19 00:42:17 -0400

    test: add location service coverage

--- test/DistributionCenter.Services.Tests/Localization/Concretes/LocationValidatorTests.cs
+++ test/DistributionCenter.Services.Tests/Localization/Concretes/LocationValidatorTests.cs
@@ -353,4 +353,151 @@ public class LocationValidatorTests
         Assert.False(result.IsSuccess);
         Assert.Equal("The identified country is not Bolivia.", result.Errors[0].Description);
     }
+
+    [Fact]
+    public async Task IsLocationInCountryAsync_IdPropertyPresentAndCorrect_ReturnsOkResult()
+    {
+        // Define Input and Output
+        GeoPoint point = new(-16.5, -68.15);
+        string jsonResponse =
+            @"{
+            ""features"": [{
+                ""context"": [
+                    { ""id"": ""country.9903"", ""text"": ""Bolivia"" }
+                ]
+            }]
+        }";
+
+        Mock<HttpMessageHandler> mockHttpMessageHandler = new();
+        _ = mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(
+                () => new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(jsonResponse) }
+            );
+
+        using HttpClient httpClient = new(mockHttpMessageHandler.Object);
+        LocationValidator locationValidator = new(httpClient, "MapboxAccessToken");
+
+        // Execute actual operation
+        Result result = await locationValidator.IsLocationInCountryAsync(point);
+
+        // Verify actual result
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task IsLocationInCountryAsync_IdPropertyPresentButDoesNotStartWithCountry_ReturnsUnexpectedError()
+    {
+        // Define Input and Output
+        GeoPoint point = new(-16.5, -68.15);
+        string jsonResponse =
+            @"{
+            ""features"": [{
+                ""context"": [
+                    { ""id"": ""city.1234"", ""text"": ""La Paz"" }
+                ]
+            }]
+        }";
+
+        Mock<HttpMessageHandler> mockHttpMessageHandler = new();
+        _ = mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(
+                () => new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(jsonResponse) }
+            );
+
+        using HttpClient httpClient = new(mockHttpMessageHandler.Object);
+        LocationValidator locationValidator = new(httpClient, "MapboxAccessToken");
+
+        // Execute actual operation
+        Result result = await locationValidator.IsLocationInCountryAsync(point);
+
+        // Verify actual result
+        Assert.False(result.IsSuccess);
+        Assert.Equal("Country property is defined but has an 'undefined' value.", result.Errors[0].Description);
+    }
+
+    [Fact]
+    public async Task IsLocationInCountryAsync_IdPropertyMissing_ReturnsUnexpectedError()
+    {
+        // Define Input and Output
+        GeoPoint point = new(-16.5, -68.15);
+        string jsonResponse =
+            @"{
+            ""features"": [{
+                ""context"": [
+                    { ""text"": ""Bolivia"" }
+                ]
+            }]
+        }";
+
+        Mock<HttpMessageHandler> mockHttpMessageHandler = new();
+        _ = mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(
+                () => new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(jsonResponse) }
+            );
+
+        using HttpClient httpClient = new(mockHttpMessageHandler.Object);
+        LocationValidator locationValidator = new(httpClient, "MapboxAccessToken");
+
+        // Execute actual operation
+        Result result = await locationValidator.IsLocationInCountryAsync(point);
+
+        // Verify actual result
+        Assert.False(result.IsSuccess);
+        Assert.Equal("Country property is defined but has an 'undefined' value.", result.Errors[0].Description);
+    }
+
+    [Fact]
+    public async Task IsLocationInCountryAsync_IdPropertyIsNull_ReturnsUnexpectedError()
+    {
+        // Define Input and Output
+        GeoPoint point = new(-16.5, -68.15);
+        string jsonResponse =
+            @"{
+            ""features"": [{
+                ""context"": [
+                    { ""id"": null, ""text"": ""Bolivia"" }
+                ]
+            }]
+        }";
+
+        Mock<HttpMessageHandler> mockHttpMessageHandler = new();
+        _ = mockHttpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(
+                () => new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(jsonResponse) }
+            );
+
+        using HttpClient httpClient = new(mockHttpMessageHandler.Object);
+        LocationValidator locationValidator = new(httpClient, "MapboxAccessToken");
+
+        // Execute actual operation
+        Result result = await locationValidator.IsLocationInCountryAsync(point);
+
+        // Verify actual result
+        Assert.False(result.IsSuccess);
+        Assert.Equal("Country property is defined but has an 'undefined' value.", result.Errors[0].Description);
+    }
 }
