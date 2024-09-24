88773047531beafb3ab3f6c9efd43b9f71f97e80
MayerliSantander <mayerlisantander0@gmail.com>
2024-09-24 14:53:05 -0400

    feat(api): modify order controller to create order post

--- src/DistributionCenter.Api/Controllers/Concretes/OrderController.cs
+++ src/DistributionCenter.Api/Controllers/Concretes/OrderController.cs
@@ -1,6 +1,8 @@
 namespace DistributionCenter.Api.Controllers.Concretes;
 
 using System.ComponentModel.DataAnnotations;
+using Commons.Errors;
+using Commons.Errors.Interfaces;
 using DistributionCenter.Api.Controllers.Bases;
 using DistributionCenter.Api.Controllers.Extensions;
 using DistributionCenter.Application.Repositories.Interfaces;
@@ -15,10 +17,13 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/orders")]
 public class OrderController(
     IRepository<Order> orderRepository,
+    IRepository<OrderProduct> orderProductRepository,
+    IRepository<Product> productRepository,
     IRepository<Client> clientRepository,
     IEmailService emailService
 ) : BaseEntityController<Order, CreateOrderDto, UpdateOrderDto>(orderRepository)
 {
+
     [HttpGet("{id}/status")]
     public async Task<IActionResult> SendEmailByStatus([FromRoute] [Required] Guid id)
     {
@@ -52,5 +57,52 @@ public class OrderController(
                 Status = order.Status.GetDescription(),
             }
         );
+    }
+
+    [HttpPost("createOrderWithNotification")]
+    public async Task<IActionResult> CreateOrder(CreateOrderDto request)
+    {
+        Result validateResult = request.Validate();
+
+        if (!validateResult.IsSuccess)
+        {
+            return this.ErrorsResponse(validateResult.Errors);
+        }
+
+        foreach (OrderProductRequestDto orderProductRequestDto in request.OrderProducts)
+        {
+            Result<Product> productResult = await productRepository.GetByIdAsync(orderProductRequestDto.ProductId);
+            if (!productResult.IsSuccess)
+            {
+                IError error = Error.NotFound();
+                List<IError> errors = new();
+                errors.Add(error);
+                return this.ErrorsResponse(errors);
+            }
+        }
+        Order entity = request.ToEntity();
+
+        Result<Order> result = await Repository.CreateAsync(entity);
+        List<OrderProduct> OrderProducts = new();
+        foreach (OrderProductRequestDto orderProductRequestDto in request.OrderProducts)
+        {
+            Result<Product> productResult = await productRepository.GetByIdAsync(orderProductRequestDto.ProductId);
+            Product product = productResult.Value;
+            CreateOrderProductDto orderProductDto = new()
+            {
+                ProductId = orderProductRequestDto.ProductId,
+                OrderId = entity.Id,
+                Quantity = orderProductRequestDto.Quantity,
+                Product = product
+            };
+            OrderProduct orderProduct = orderProductDto.ToEntity();
+            OrderProducts.Add(orderProduct);
+            _ = await orderProductRepository.CreateAsync(orderProduct);
+        }
+
+        entity.Products = OrderProducts;
+
+        _ = SendEmailByStatus(entity.Id);
+        return result.Match(entity => Ok(entity), this.ErrorsResponse);
     }
 }
