7419a93571dba7d129508c551a1099ff0d81b1e9
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-09-26 23:46:59 -0400

    feat(services): update al services to fix some ishues

--- src/DistributionCenter.Services/Localization/Commons/GeoPoint.cs
+++ src/DistributionCenter.Services/Localization/Commons/GeoPoint.cs
@@ -2,6 +2,28 @@ namespace DistributionCenter.Services.Localization.Commons;
 
 public class GeoPoint(double latitude, double longitude)
 {
-    public double Latitude { get; set; } = latitude;
-    public double Longitude { get; set; } = longitude;
+    private const double Tolerance = 0.0000001;
+    public double Latitude { get; } = latitude;
+    public double Longitude { get; } = longitude;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not GeoPoint other) return false;
+        return Math.Abs(Latitude - other.Latitude) < Tolerance
+               && Math.Abs(Longitude - other.Longitude) < Tolerance;
+    }
+
+    public override int GetHashCode()
+    {
+        int latHash = (Latitude / Tolerance).GetHashCode();
+        int lonHash = (Longitude / Tolerance).GetHashCode();
+
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 23 + latHash;
+            hash = hash * 23 + lonHash;
+            return hash;
+        }
+    }
 }
