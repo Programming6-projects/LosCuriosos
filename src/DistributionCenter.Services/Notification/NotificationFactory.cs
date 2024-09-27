92fbf784b509a7282b218b236394c0ea14b12170
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-09-27 02:38:41 -0400

    refactor(*): update implementation of notification factory to manage a dto instad many parameters

--- src/DistributionCenter.Services/Notification/NotificationFactory.cs
+++ src/DistributionCenter.Services/Notification/NotificationFactory.cs
@@ -2,26 +2,24 @@ namespace DistributionCenter.Services.Notification;
 
 using Concretes;
 using Domain.Entities.Enums;
+using Dtos;
 using Interfaces;
 
 public static class NotificationFactory
 {
-    private static readonly Dictionary<Status, Func<Guid, IMessage>> MessageMap =
+    private static readonly Dictionary<Status, Func<OrderDto, IMessage>> MessageMap =
         new()
         {
-            { Status.Pending, static orderId => new OrderConfirmationMessage(orderId) },
-            { Status.Sending, static orderId => new OrderShippedMessage(orderId) },
-            { Status.Delivered, static orderId => new OrderDeliveredMessage(orderId) },
-            { Status.Cancelled, static orderId => new OrderCancelledMessage(orderId) },
+            { Status.Pending, static order => new OrderConfirmationMessage(order) },
+            { Status.Sending, static order => new OrderShippedMessage(order) },
+            { Status.Delivered, static order => new OrderDeliveredMessage(order) },
+            { Status.Cancelled, static order => new OrderCancelledMessage(order) },
         };
 
-    public static IMessage CreateMessage(Status status, Guid orderId)
+    public static IMessage CreateMessage(OrderDto order)
     {
-        if (MessageMap.TryGetValue(status, out Func<Guid, IMessage>? messageCreator))
-        {
-            return messageCreator(orderId);
-        }
-
-        throw new ArgumentOutOfRangeException(nameof(status), $"Unsupported order status: {status}");
+        if (MessageMap.TryGetValue(order.OrderStatus, out Func<OrderDto, IMessage>? messageCreator))
+            return messageCreator(order);
+        return null!;
     }
 }
